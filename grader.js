6c788965b3d890f2b03769645160f2530363a516
xuyuhang <xuyuhang1005@gmail.com>
2013-07-08 17:46:52 -0700

    Add wireframe of the website. Add grader.js.

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -55,6 +56,17 @@ var checkHtmlFile = function(htmlfile, checksfile) {
   return out;
 };
 
+var checkHtmlString = function(htmlString, checksfile) {
+  $ = cheerio.load(htmlString);
+  var checks = loadChecks(checksfile).sort();
+  var out = {};
+  for(var ii in checks) {
+    var present = $(checks[ii]).length > 0;
+    out[checks[ii]] = present;
+  }
+  return out;
+}
+
 var clone = function(fn) {
   // Workaround for commander.js issue.
   // http://stackoverflow.com/a/6772648
@@ -65,10 +77,23 @@ if(require.main == module) {
   program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+    .option('-u, --url <html_url>', 'URL of index.html')
     .parse(process.argv);
-  var checkJson = checkHtmlFile(program.file, program.checks);
-  var outJson = JSON.stringify(checkJson, null, 4);
-  console.log(outJson);
+  var file = program.file;
+  if (program.url) {
+    rest.get(program.url).on('complete', function(result) {
+      if (result instanceof Error) {
+        console.log('Error: ' + result.message);
+      } else {
+        var outJson = checkHtmlString(result, program.checks);
+        console.log(outJson);
+      }
+    });
+  } else {
+    var checkJson = checkHtmlFile(program.file, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+  }
 } else {
   exports.checkHtmlFile = checkHtmlFile;
 }
